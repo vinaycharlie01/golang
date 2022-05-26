bb366b4aa8b868b2aba6a61692d1a13a65398c8b
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-05-26 11:15:12 +0530

    Update main.go

--- fuction/main.go
+++ fuction/main.go
@@ -1,1 +1,28 @@
+package main
+
+import "fmt"
+
+func foo(a ...int) int {
+	val := 0
+	for _, v := range a {
+		val += v
+
+	}
+	return val
+}
+func foo1(b []int) int {
+	val := 0
+	for _, v := range b {
+		val += v
+
+	}
+	return val
+}
+
+func main() {
+	a := []int{10, 20, 30, 30, 50, 60}
+	b := foo(a...)
+	d := foo1(a)
+	fmt.Println(b, d)
+}
 
