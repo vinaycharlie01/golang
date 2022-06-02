d14f36192c431db22b7ae3ae18bba0a1461998f5
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-02 16:27:44 +0530

    Create vinayinterface.go

---
+++ golang master class/interface/vinayinterface.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type shape interface {
+	area() float64
+	perimeter() float64
+}
+
+type rectangle struct {
+	width, height float64
+}
+type circle struct {
+	radius float64
+}
+
+func (c circle) area() float64 {
+	return math.Pi * math.Pow(c.radius, 2)
+}
+
+// method that calculates rectangle's area
+func (r rectangle) area() float64 {
+	return r.height * r.width
+}
+
+func (c circle) perimeter() float64 {
+	return 2 * math.Pi * c.radius
+}
+
+func (r rectangle) perimeter() float64 {
+	return 2 * (r.height + r.width)
+}
+
+func print(s shape) {
+	fmt.Printf("Shape: %#v\n", s)
+	fmt.Printf("Area: %v\n", s.area())
+	fmt.Printf("Perimeter: %v\n", s.perimeter())
+}
+
+func main() {
+
+	c1 := circle{radius: 5}
+	r1 := rectangle{width: 3, height: 2}
+
+	print(c1)
+	print(r1)
+
+	a := c1.area()
+	fmt.Println("Circle Area:", a)
+}
