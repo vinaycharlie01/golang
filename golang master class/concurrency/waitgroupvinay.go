84d3f267da74d1705a7e092b0a365b64e488ecd0
vinaycharlie01 <106230026+vinaycharlie01@users.noreply.github.com>
2022-06-03 15:31:20 +0530

    Create waitgroupvinay.go

---
+++ golang master class/concurrency/waitgroupvinay.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func f1(wg *sync.WaitGroup) {
+	fmt.Println("f1(goroutine) execution started")
+	for i := 0; i < 3; i++ {
+		fmt.Println("f1, i=", i)
+		time.Sleep(time.Second)
+
+	}
+	fmt.Println("f1 execution finished")
+	wg.Done()
+	//or:
+	// (*wg).Done()
+}
+
+func f2() {
+	fmt.Println("f2 execution started")
+	time.Sleep(time.Second)
+
+	for i := 5; i < 8; i++ {
+		fmt.Println("f2(), i=", i)
+
+	}
+	fmt.Println("f2 execution finished")
+
+}
+
+func main() {
+	fmt.Println("main execution started")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go f1(&wg)
+	fmt.Println("No. of Goroutines:", runtime.NumGoroutine())
+	f2()
+	wg.Wait()
+
+	fmt.Println("main execution stopped")
+}
